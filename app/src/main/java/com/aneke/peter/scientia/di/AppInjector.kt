709638dfb8884b359bf96b50516e761a646302c8
petyr47 <anekepeter47@gmail.com>
2020-02-17 17:21:43 +0100

    setup appinjector

--- app/src/main/java/com/aneke/peter/scientia/di/AppInjector.kt
+++ app/src/main/java/com/aneke/peter/scientia/di/AppInjector.kt
@@ -1,15 +1,24 @@
 package com.aneke.peter.scientia.di
 
+import com.aneke.peter.scientia.data.AppDatabase
+import com.aneke.peter.scientia.data.PrefStore
+import com.aneke.peter.scientia.movie.MovieRepository
+import com.aneke.peter.scientia.movie.MovieViewModel
+import com.aneke.peter.scientia.network.RetrofitClient
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val dataModule = module {
+    single { AppDatabase.getInstance(get()) }
+    single { RetrofitClient.makeApiService() }
+    single { PrefStore(get()) }
 
 }
 
 val viewModels = module {
-
+    viewModel { MovieViewModel(get()) }
 }
 
 val repositories = module {
-
+    single { MovieRepository(get(), get(), get()) }
 }
