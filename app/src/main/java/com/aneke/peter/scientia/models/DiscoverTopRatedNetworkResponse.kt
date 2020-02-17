467c29568c333172e6c84aa7ceb2fedb0324d97d
petyr47 <anekepeter47@gmail.com>
2020-02-17 09:42:30 +0100

    create required data structures

---
+++ app/src/main/java/com/aneke/peter/scientia/models/DiscoverTopRatedNetworkResponse.kt
@@ -0,0 +1,21 @@
+package com.aneke.peter.scientia.models
+
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class DiscoverTopRatedNetworkResponse(
+
+    @Json(name = "page")
+    val page: Int = 0, // 1
+
+    @Json(name = "results")
+    val results: List<Result> = listOf(),
+
+    @Json(name = "total_pages")
+    val totalPages: Int = 0,
+
+    @Json(name = "total_results")
+    val totalResults: Int = 0
+)
---
+++ app/src/main/java/com/aneke/peter/scientia/models/NetworkResponse.kt
@@ -0,0 +1,3 @@
+package com.aneke.peter.scientia.models
+
+data class NetworkResponse(val data : DiscoverTopRatedNetworkResponse? = null, var message : String = " ", var status : Boolean = false)
