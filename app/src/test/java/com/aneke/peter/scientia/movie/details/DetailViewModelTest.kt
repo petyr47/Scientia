7f8176b3e3a039ea46a6099fb08cfc7c303d6610
petyr47 <anekepeter47@gmail.com>
2020-02-19 09:52:29 +0100

    add test classes

---
+++ app/src/test/java/com/aneke/peter/scientia/movie/details/DetailViewModelTest.kt
@@ -0,0 +1,13 @@
+package com.aneke.peter.scientia.movie.details
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class DetailViewModelTest{
+
+    @Test
+    fun testViewModel(){
+        assertEquals(4 , 2*2)
+    }
+
+}
