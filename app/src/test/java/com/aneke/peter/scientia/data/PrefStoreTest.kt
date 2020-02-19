f9a3ae58175c27690098a7bb258423d9388a3abc
petyr47 <anekepeter47@gmail.com>
2020-02-19 17:07:17 +0100

    add unit tests

--- app/src/test/java/com/aneke/peter/scientia/data/PrefStoreTest.kt
+++ app/src/test/java/com/aneke/peter/scientia/data/PrefStoreTest.kt
@@ -1,7 +1,80 @@
 package com.aneke.peter.scientia.data
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+import org.mockito.junit.MockitoJUnit
+import org.mockito.junit.MockitoRule
+import java.util.*
 
+@RunWith(JUnit4::class)
 class PrefStoreTest {
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @Mock
+    private lateinit var prefStore: PrefStore
+
+
+    @Rule
+    @JvmField
+    val mockitoRule: MockitoRule = MockitoJUnit.rule()
+
+
+    @Before
+    fun before() {
+        MockitoAnnotations.initMocks(this)
+
+    }
+
+
+    @Test
+    fun testThatSholdRefreshDataWouldReturnTrueIfLastSyncTimeIsZero(){
+        Mockito.`when`(prefStore.lateSyncTime).thenReturn(
+            0
+        ).then {
+            assertTrue(prefStore.shouldRefreshData())
+        }
+    }
+
+
+    @Test
+    fun testThatSholdRefreshDataWouldReturnFalseIfLastSyncTimeIsNow(){
+        Mockito.`when`(prefStore.lateSyncTime).thenReturn(
+            Calendar.getInstance().timeInMillis
+        ).then {
+            assertFalse(prefStore.shouldRefreshData())
+        }
+    }
+
+    @Test
+    fun testThatSholdRefreshDataWouldReturnFalseIfLastSyncTimeIsTenMinutesBeforeNow(){
+        Mockito.`when`(prefStore.lateSyncTime).thenReturn(
+            Calendar.getInstance().timeInMillis - (10 * 60 * 1000)
+        ).then {
+            assertFalse(prefStore.shouldRefreshData())
+        }
+    }
+
+
+    @Test
+    fun testThatSholdRefreshDataWouldReturnTrueIfLastSyncTimeIsOneDayBeforeNow(){
+        Mockito.`when`(prefStore.lateSyncTime).thenReturn(
+            Calendar.getInstance().timeInMillis - (24 * 60 * 60 * 1000)
+        ).then {
+            assertTrue(prefStore.shouldRefreshData())
+        }
+    }
+
+
+
 
 }
--- app/src/test/java/com/aneke/peter/scientia/movie/details/DetailViewModelTest.kt
+++ app/src/test/java/com/aneke/peter/scientia/movie/details/DetailViewModelTest.kt
@@ -1,13 +1,100 @@
 package com.aneke.peter.scientia.movie.details
 
+
+import com.aneke.peter.scientia.models.Movie
+import com.aneke.peter.scientia.movie.MovieRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Test
+import org.koin.test.KoinTest
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
 
-class DetailViewModelTest{
 
-    @Test
-    fun testViewModel(){
-        assertEquals(4 , 2*2)
+class DetailViewModelTest : KoinTest {
+
+    private lateinit var detailViewModel: DetailViewModel
+
+    @Mock
+    private lateinit var repository: MovieRepository
+
+    val id = 8
+
+
+    @Before
+    fun before() {
+        MockitoAnnotations.initMocks(this)
+        detailViewModel = DetailViewModel(repository)
+
     }
+
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun testThatMovieLiveDataValueIsNotNullAfterCallToFetchMovieDetails() = runBlockingTest {
+        Mockito.`when`(repository.fetchMovie(id)).thenReturn(
+            Movie()
+        ).then {
+            assertNotNull(detailViewModel.movie.value)
+        }
+    }
+
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun testThatMovieLiveDataValueIsNullAfterCallToFetchMovieDetailsReturnsNull() =
+        runBlockingTest {
+            Mockito.`when`(repository.fetchMovie(id)).thenReturn(
+                null
+            ).then {
+                assertNull(detailViewModel.movie.value)
+            }
+        }
+
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun testThatMovieDetailsLiveDataValuesAreNullAfterCallToFetchMovieDetailsReturnsNull() =
+        runBlockingTest {
+            Mockito.`when`(repository.fetchMovie(id)).thenReturn(
+                null
+            ).then {
+                assertNull(detailViewModel.movieBackdrop.value)
+                assertNull(detailViewModel.movieDesc.value)
+                assertNull(detailViewModel.moviePoster.value)
+                assertNull(detailViewModel.movieReleaseDate.value)
+                assertNull(detailViewModel.movieRating.value)
+                assertNull(detailViewModel.movieTitle.value)
+            }
+        }
+
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun testThatMovieDetailsLiveDataValuesAreUpdatedCorrectlyAfterCallToFetchMovieDetailsReturnAAvalid() =
+        runBlockingTest {
+            Mockito.`when`(repository.fetchMovie(id)).thenReturn(
+                Movie(
+                    backdropPath = "backdropurl",
+                    overview = "Movie overview",
+                    posterPath = "Poster path",
+                    releaseDate = "Date of release",
+                    voteAverage = 90.0,
+                    title = "Game of thrones"
+
+                )
+            ).then {
+                assertEquals(detailViewModel.movieBackdrop.value, "backdropurl")
+                assertNull(detailViewModel.movieDesc.value, "Movie overview")
+                assertNull(detailViewModel.moviePoster.value, "Poster path")
+                assertNull(detailViewModel.movieReleaseDate.value, "Date of release")
+                assertNull(detailViewModel.movieRating.value, 90.0)
+                assertNull(detailViewModel.movieTitle.value, "Game of thrones" )
+            }
+        }
+
 
 }
